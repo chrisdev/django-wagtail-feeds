d479df6ce2abe026483fd9d5e84feaa48ddf20ba
Parbhat Puri <parbhatpuri17@gmail.com>
2016-06-23 14:33:38 +0530

    migrations and docs updated

--- wagtail_feeds/migrations/0001_initial.py
+++ wagtail_feeds/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('wagtailcore', '0028_merge'),
+
     ]
 
     operations = [
--- wagtail_feeds/migrations/0003_auto_20160620_1301.py
+++ wagtail_feeds/migrations/0003_auto_20160620_1301.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('feeds', '0002_auto_20160620_1155'),
+        ('wagtail_feeds', '0002_auto_20160620_1155'),
     ]
 
     operations = [
