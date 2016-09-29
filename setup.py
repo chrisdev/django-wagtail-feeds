34f49199c328e4b20fa97aae4a2eae06a7a1b0db
Christopher Clarke <cclarke@chrisdev.com>
2016-09-29 13:27:30 -0400

    Latest release

--- setup.py
+++ setup.py
@@ -12,15 +12,13 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    "Django>=1.8,<1.11",
+    "Django>=1.8",
     "Wagtail>=1.4",
-    "html5lib==0.999999",
 ]
 
 test_requirements = [
-    "Django>=1.8,<1.11",
+    "Django>=1.8",
     "Wagtail>=1.4",
-    "html5lib==0.999999",
 ]
 
 setup(
