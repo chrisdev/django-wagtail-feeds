ac2270b83adb8b2b8126ddbd63299875273b00be
Parbhat Puri <parbhatpuri17@gmail.com>
2016-10-03 17:44:00 +0530

    Test for StreamField Issue #24

--- tests/models.py
+++ tests/models.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
+from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
@@ -13,6 +15,23 @@ class HomePage(Page):
 class BlogPage(Page):
     intro = RichTextField()
     body = RichTextField()
+    date = models.DateField("Post date")
+
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+class BlogStreamPage(Page):
+    intro = RichTextField()
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
     date = models.DateField("Post date")
 
     feed_image = models.ForeignKey(
